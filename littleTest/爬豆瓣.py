0a21faf6fff94dc07c59ad1853fa13cb44981ab3
zhangwei <121948052@qq.com>
2024-10-11 10:35:34 +0800

    fix: use openpyxl

--- littleTest/爬豆瓣.py
+++ littleTest/爬豆瓣.py
@@ -1,11 +1,6 @@
-'''
-Author: Bug Router
-Date: 2024-09-29 14:54:31
-Description: Default
-'''
 import requests
 from bs4 import BeautifulSoup
-import xlwt
+import openpyxl
 
 
 def request_douban(url):
@@ -22,27 +17,17 @@ def request_douban(url):
         return None
 
 
-book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+def save_to_excel(soup, workbook):
+    sheet = workbook.active
+    sheet.title = '豆瓣电影Top250'
+    sheet.append(['名称', '图片', '排名', '评分', '作者', '简介'])
 
-sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=True)
-sheet.write(0, 0, '名称')
-sheet.write(0, 1, '图片')
-sheet.write(0, 2, '排名')
-sheet.write(0, 3, '评分')
-sheet.write(0, 4, '作者')
-sheet.write(0, 5, '简介')
-
-n = 1
-
-
-def save_to_excel(soup):
     movie_list = soup.find(class_='grid_view').find_all('li')
 
     for item in movie_list:
         item_name = item.find(class_='title').string
         item_img = item.find('a').find('img').get('src')
-        # 这里假设排名在每个 `li` 元素中有一个特定的位置或特征，可以根据实际情况修改查找方式
-        item_index = item.find_something_to_get_rank()
+        item_index = item.find('em').string
         item_score = item.find(class_='rating_num').string
         item_author = item.find('p').text
         if item.find(class_='inq') is not None:
@@ -50,31 +35,20 @@ def save_to_excel(soup):
         else:
             item_intr = 'NOT AVAILABLE'
 
-        # print('爬取电影：' + item_index + ' | ' + item_name +' | ' + item_img +' | ' + item_score +' | ' + item_author +' | ' + item_intr )
         print('爬取电影：' + item_index + ' | ' + item_name + ' | ' + item_score + ' | ' + item_intr)
 
-        global n
-
-        sheet.write(n, 0, item_name)
-        sheet.write(n, 1, item_img)
-        sheet.write(n, 2, item_index)
-        sheet.write(n, 3, item_score)
-        sheet.write(n, 4, item_author)
-        sheet.write(n, 5, item_intr)
-
-        n = n + 1
+        sheet.append([item_name, item_img, item_index, item_score, item_author, item_intr])
 
 
 def main(page):
     url = 'https://movie.douban.com/top250?start=' + str(page * 25) + '&filter='
     html = request_douban(url)
     soup = BeautifulSoup(html, 'lxml')
-    save_to_excel(soup)
+    workbook = openpyxl.Workbook()
+    save_to_excel(soup, workbook)
+    workbook.save(u'豆瓣最受欢迎的250部电影.xlsx')
 
 
 if __name__ == '__main__':
-
     for i in range(0, 10):
         main(i)
-
-book.save(u'豆瓣最受欢迎的250部电影.xlsx')
